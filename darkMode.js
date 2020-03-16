d05fac69ee7242b842b01bae629ccffbd59d98a2
Safnas <iamsafnas@gmail.com>
2020-03-16 18:12:12 +0530

    Update darkMode.js

--- darkMode.js
+++ darkMode.js
@@ -42,15 +42,15 @@ window.onload = function switchMode() {
     }
 }
 
-// var prevScrollpos = window.pageYOffset;
-// window.onscroll = function () {
-//     var currentScrollPos = window.pageYOffset;
-//     if (currentScrollPos > 0) {
-//         if (prevScrollpos > currentScrollPos) {
-//             document.getElementById("navigation").style.top = "0";
-//         } else {
-//             document.getElementById("navigation").style.top = "-70px";
-//         }
-//         prevScrollpos = currentScrollPos;
-//     }
-// }
+ var prevScrollpos = window.pageYOffset;
+ window.onscroll = function () {
+     var currentScrollPos = window.pageYOffset;
+     if (currentScrollPos > 0) {
+         if (prevScrollpos > currentScrollPos) {
+             document.getElementById("navigation").style.top = "0";
+        } else {
+            document.getElementById("navigation").style.top = "-70px";
+         }
+         prevScrollpos = currentScrollPos;
+     }
+ }
