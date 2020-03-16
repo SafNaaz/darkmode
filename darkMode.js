e500062e81dac1248cdab8837559ff81d026f770
Safnas <iamsafnas@gmail.com>
2020-03-16 17:55:53 +0530

    Add files via upload

--- darkMode.js
+++ darkMode.js
@@ -12,7 +12,6 @@ function toggleDarkMode() {
     }
 }
 
-// Code for smooth scroll
 let anchorlinks = document.querySelectorAll('a[href^="#"]')
 
 for (let item of anchorlinks) {
@@ -20,9 +19,9 @@ for (let item of anchorlinks) {
         let hashval = item.getAttribute('href')
         let target = document.querySelector(hashval)
         target.scrollIntoView({
-            behavior: 'smooth'
-            // block: 'start',
-            // inline:'nearest'
+            behavior: 'smooth',
+            block: 'start',
+            inline:'nearest'
         })
         history.pushState(null, null, hashval) // for url updation
         e.preventDefault() //needed for scroll
@@ -30,28 +29,28 @@ for (let item of anchorlinks) {
 }
 //new codes add below
 
-//Code for switching to dark mode at specific times
-window.onload = function(){
-var today = new Date()
-var time = today.getHours()
-if(time < 6 || time >= 20){
-   var body = document.getElementById('body')
-   var button = document.getElementById('dark-mode-button')
-   body.className = 'body-dark'
-   button.innerHTML = '🌞'
-   button.title = 'lights on'
-  }
+
+window.onload = function switchMode() {
+    var today = new Date()
+    var time = today.getHours()
+    if (time < 6 || time > 20) {
+        var body = document.getElementById('body')
+        var button = document.getElementById('dark-mode-button')
+        body.className = 'body-dark'
+        button.innerHTML = '🌞'
+        button.title = 'lights on'
+    }
 }
 
-//Code for hiding nav bar
-
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navigation").style.top = "0";
-  } else {
-    document.getElementById("navigation").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
-}
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function () {
+//     var currentScrollPos = window.pageYOffset;
+//     if (currentScrollPos > 0) {
+//         if (prevScrollpos > currentScrollPos) {
+//             document.getElementById("navigation").style.top = "0";
+//         } else {
+//             document.getElementById("navigation").style.top = "-70px";
+//         }
+//         prevScrollpos = currentScrollPos;
+//     }
+// }
