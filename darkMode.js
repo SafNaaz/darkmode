6579fb7f1ee343b9652802318c6e33b09f82412e
Safnas <iamsafnas@gmail.com>
2020-03-18 17:37:42 +0530

    Add files via upload

--- darkMode.js
+++ darkMode.js
@@ -1,35 +1,60 @@
+/* dark mode code */
+const darkModeButtonId = 'dark-mode-button';
+
+function darkMode() {
+    let button = document.getElementById(darkModeButtonId);
+    document.body.className = 'body-dark';
+    button.innerHTML = '🌞';
+    button.title = 'lights on';
+}
+
+function lightMode() {
+    let button = document.getElementById(darkModeButtonId);
+    document.body.className = 'body-light';
+    button.innerHTML = '🌙';
+    button.title = 'dark mode';
+}
 
 function toggleDarkMode() {
-    var body = document.getElementById('body')
-    var button = document.getElementById('dark-mode-button')
-	if (body.className == 'body-light') {
-        body.className = 'body-dark'
-        button.innerHTML = '🌞'
-        button.title = 'lights on'
-	setMode('dark')
+    if (document.body.className == 'body-light') {
+        darkMode()
+        setModeInSession('dark');
     } else {
-        body.className = 'body-light'
-        button.innerHTML = '🌙'
-        button.title = 'dark mode'
-	setMode('light')
+        lightMode()
+        setModeInSession('light');
     }
 }
 
-
-function setMode(value) {
-   if (sessionStorage.getItem('mode') != value) {
-       sessionStorage.setItem('mode', value);
-  }
-}
-
-function getMode() {
-    myValue = null;
-    if (sessionStorage.getItem('mode')) {
-        myValue = sessionStorage.getItem('mode');
+// to save the state of the mode to appear across pages in current browser session
+function setModeInSession(value) {
+    if (sessionStorage.getItem('mode') != value) {
+        sessionStorage.setItem('mode', value);
     }
-     return myValue;
 }
 
+function getModeFromSession() {
+    return sessionStorage.getItem('mode');
+}
+
+window.onload = function switchMode() {
+    if (!(getModeFromSession() === null)) { //get the mode and apply for current page
+        if (getModeFromSession() == 'dark') {
+            darkMode()
+        } else {
+            lightMode()
+        }
+    } else { //if no mode in session apply dark mode based on current time
+        let today = new Date()
+        let time = today.getHours()
+        if (time < 6 || time > 20) {
+            darkMode()
+        }
+    }
+}
+
+/* dark mode code - ends */
+
+/* scroll to anchor */
 let anchorlinks = document.querySelectorAll('a[href^="#"]')
 
 for (let item of anchorlinks) {
@@ -37,90 +62,62 @@ for (let item of anchorlinks) {
         let hashval = item.getAttribute('href')
         let target = document.querySelector(hashval)
         target.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start',
-            inline:'nearest'
+            behavior: 'smooth'
+            //block: 'start',
+            //inline:'nearest'
         })
         history.pushState(null, null, hashval) // for url updation
         e.preventDefault() //needed for scroll
     })
 }
-//new codes add below
+/* scroll to anchor - ends */
 
+/* go to top button */
 
-window.onload = function switchMode() {
-	
-	if(!(getMode() === null)){
-		var body = document.getElementById('body')
-    var button = document.getElementById('dark-mode-button')
-	if (getMode() == 'dark') {
-        body.className = 'body-dark'
-        button.innerHTML = '🌞'
-        button.title = 'lights on'
-	
-    } else {
-        body.className = 'body-light'
-        button.innerHTML = '🌙'
-        button.title = 'dark mode'
-	
+var prevScrollpos = window.pageYOffset;
+const navigationBarId = 'navigation';
+const footerId = 'footer';
+window.onscroll = function () {
+    let currentScrollPos = window.pageYOffset;
+    if (currentScrollPos > 0) {
+        if (prevScrollpos > currentScrollPos) {
+            document.getElementById(navigationBarId).style.top = "0";
+            document.getElementById(footerId).style.bottom = "0";
+        } else {
+            document.getElementById(navigationBarId).style.top = "-70px";
+            document.getElementById(footerId).style.bottom = "-70px";
+        }
+        prevScrollpos = currentScrollPos;
     }
-	}else{
-		var today = new Date()
-		var time = today.getHours()
-		if (time < 6 || time > 20) {
-			var body = document.getElementById('body')
-			var button = document.getElementById('dark-mode-button')
-			body.className = 'body-dark'
-			button.innerHTML = '🌞'
-			button.title = 'lights on'
-		}
-	}
+    scrollFunction();
 }
-
-
- var prevScrollpos = window.pageYOffset;
- window.onscroll = function () {
-     var currentScrollPos = window.pageYOffset;
-     if (currentScrollPos > 0) {
-         if (prevScrollpos > currentScrollPos) {
-             document.getElementById("navigation").style.top = "0";
-             document.getElementById("footer").style.bottom = "0";
-         } else {
-             document.getElementById("navigation").style.top = "-70px";
-             document.getElementById("footer").style.bottom = "-70px";
-         }
-         prevScrollpos = currentScrollPos;
-     }
-	 scrollFunction();
- }
- 
- 
- //Get the button:
-mybutton = document.getElementById("goToTopBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 //window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
+    let goToTopButton = document.getElementById("goToTopBtn");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        goToTopButton.style.display = "block";
+    } else {
+        goToTopButton.style.display = "none"; // to disappear once scroll ends
+    }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function goToTopFunction() {
-	scrollToTop();
-  //document.body.scrollTop = 0; // For Safari
- // document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    scrollToTop();
+    //document.body.scrollTop = 0; // For Safari
+    // document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
 const scrollToTop = () => {
-  const c = document.documentElement.scrollTop || document.body.scrollTop;
-  if (c > 0) {
-    window.requestAnimationFrame(scrollToTop);
-    window.scrollTo(0, c - c / 8);
-  }
+    const c = document.documentElement.scrollTop || document.body.scrollTop;
+    if (c > 0) { // check if page reached top or not
+        window.requestAnimationFrame(scrollToTop); // for smooth animation
+        window.scrollTo(0, c - c / 8);
+    }
 };
+
+/* go to top button - ends*/
 
