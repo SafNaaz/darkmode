a687306f4090419f79b1f61231cca55f338c29d2
Safnas <iamsafnas@gmail.com>
2020-03-19 22:35:25 +0530

    Update darkMode.js

--- darkMode.js
+++ darkMode.js
@@ -111,9 +111,9 @@ function scrollFunction() {
 
 // When the user clicks on the button, scroll to the top of the document
 function goToTopFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     scrollToTop();
-    //document.body.scrollTop = 0; // For Safari
-    // document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
 const scrollToTop = () => {
